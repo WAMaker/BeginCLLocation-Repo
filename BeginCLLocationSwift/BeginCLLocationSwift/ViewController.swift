1b7d3a75bef5dfa81ea79d8ffec5279b4c0e75a6
WAMaker <wamakerdevelop@gmail.com>
2015-08-11 01:02:47 +0800

    upload BeginCLLocationSwift

---
+++ BeginCLLocationSwift/BeginCLLocationSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeginCLLocationSwift
+//
+//  Created by wamaker on 15/8/11.
+//  Copyright (c) 2015年 wamaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
